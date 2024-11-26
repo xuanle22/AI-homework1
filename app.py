e1f07e565c4f6e3c03cd598141f390a8c0b0779a
xuanle22 <49552696+xuanle22@users.noreply.github.com>
2024-11-26 08:42:17 +0000

    Nov 26th v1

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask
 from flask import render_template, request
 
-app = Flask("__name__") ### 这里"app"最好和文件名一致，"__": 在手机上使用时如果有web scam, creater will look for you
+app = Flask("__name__") 
 
 
-@app.route("/",methods =["GET","POST"]) #/是staring point,
+@app.route("/",methods =["GET","POST"])
 def index():
-    return(render_template("index.html")) ###111
+    return(render_template("index.html")) 
 
-@app.route("/main",methods =["GET","POST"]) #/是staring point,
-def main():
-    name = request.form.get("q")
-    return(render_template("main.html")) ###111
 
-if __name__ == "__main__": #magic name, "__": for system,我们不能用于variable name， double confirm：writing+responsible, responsable
+
+if __name__ == "__main__": 
     app.run() 
 
 
